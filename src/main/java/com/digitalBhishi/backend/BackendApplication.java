be17803641afd603bda6d99f5061d5c155da2667
Gourab Karmakar <gourabk7458@gmail.com>
2023-02-20 00:43:38 +0530

    pom changes => maven installs npm and node locally

--- src/main/java/com/digitalBhishi/backend/BackendApplication.java
+++ src/main/java/com/digitalBhishi/backend/BackendApplication.java
@@ -1,13 +1,31 @@
 package com.digitalBhishi.backend;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication app = new SpringApplication(BackendApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        app.run(args);
+		//SpringApplication.run(BackendApplication.class, args);
 	}
+	
+}
+@Component
+class ServerPortCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+ 
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(8081);
+    }
+
 
 }
