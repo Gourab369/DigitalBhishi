76a465ff7f3eb4a95580de447896ed49836e5635
Gourab Karmakar <108367199+Gourab369@users.noreply.github.com>
2023-03-13 16:51:33 +0530

    Update AdminCustomers.jsx

--- frontend/src/Components/adminDashboard/AdminCustomers.jsx
+++ frontend/src/Components/adminDashboard/AdminCustomers.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from 'react'
 import Navbar from '../Navbar';
-import backgroundImage from "./backdashboard.jpg"
 import http from '../../httpCommon'
 
 const AdminCustomers = () => {
