636816feb47d3defba9f503a1dc92ddffc785b9b
Gourab Karmakar <108367199+Gourab369@users.noreply.github.com>
2023-03-13 12:53:33 +0530

    Delete frontend/src directory

--- frontend/src/Components/Missing.jsx
+++
@@ -1,9 +0,0 @@
-const Missing =()=>{
-    return (
-        <>
-            <h1 style={{fontSize:"40px", width:"100vw"}} className="text-center">404 Missing</h1>
-        </>
-    );
-}
-
-export default Missing;
